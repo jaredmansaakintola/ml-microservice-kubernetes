8bfef0e693745afaf2015eed1369abdfb4f4129e
Jared Godfrey <jaredgodfrey@UDPCorpAdmins-MacBook-Pro.local>
2020-04-01 19:38:29 -0700

    finished testing prediction algorithm and testing depliy to kubernetes for docker container

---
+++ run_docker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build --tag jaredg/ml-microservice:latest .
+
+# Step 2: 
+# List docker images
+docker images ls
+
+# Step 3: 
+# Run flask app
+docker run -p 8000:80 --name ml-microservice jaredg/ml-microservice:latest
+
---
+++ run_kubernetes.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=jaredg/ml-microservice
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run ml-microservice --generator=run-pod/v1 --image=$dockerpath --port=80 --labels app=ml-microservice 
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward ml-microservice 8000:80
